7114bc5527655586b7e729915161ff1bb1fe2033
Erwin Antepuesto <matcha.ccy@gmail.com>
2023-11-14 01:09:31 +0800

    [sciyon]Finished and tested routes for register and login

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import compression from 'compression';
 import cors from 'cors';
 import mongoose, { mongo } from 'mongoose';
 
+import router from './router';
+
 const app = express();
 
 app.use(cors({
@@ -19,7 +21,7 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 
 server.listen(8080, () => {
-  console.log('Server running on http://localhost:8000/');
+  console.log('Server running on http://localhost:8080/');
 })
 
 const MONGO_URL = 'mongodb+srv://matchaccy:BeC1CiYYQpZ2ypX2@cluster0.6hxebjk.mongodb.net/?retryWrites=true&w=majority'
@@ -29,3 +31,4 @@ mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error));
 
 
+app.use('/', router());
